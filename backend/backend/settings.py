355b38c3d096dfd5b44b0b038867d99e65178a77
alexey <alexdream1992@yandex.ru>
2023-07-26 22:33:15 +0400

    3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,23 +57,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-
-
 DATABASES = {
-  'default': { 
-     'ENGINE': 'django.db.backends.postgresql', 
-     'NAME': os.getenv('POSTGRES_DB', 'django'), 
-     'USER': os.getenv('POSTGRES_USER', 'django'), 
-     'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''), 
-     'HOST': os.getenv('DB_HOST', ''), 
-     'PORT': os.getenv('DB_PORT', 5432) 
-   } 
+  'default': {
+     'ENGINE': 'django.db.backends.postgresql',
+     'NAME': os.getenv('POSTGRES_DB', 'django'),
+     'USER': os.getenv('POSTGRES_USER', 'django'),
+     'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+     'HOST': os.getenv('DB_HOST', ''),
+     'PORT': os.getenv('DB_PORT', 5432)
+   }
  }
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
