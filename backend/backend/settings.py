784617e1c12d6c4d26e9d3ff1482b107b384d214
alexey <alexdream1992@yandex.ru>
2023-08-07 08:19:42 +0400

    gds

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
-ALLOWED_HOSTS = ['158.160.9.166', '127.0.0.1', 'localhost' ,'alexey-yp.myddns.me']
+#ALLOWED_HOSTS = ['158.160.9.166', '127.0.0.1', 'localhost' ,'alexey-yp.myddns.me']
+
+ALLOWED_HOSTS = ['alexey-yp.myddns.me']
 
 # Application definition
 
